29e95deb05c4cf44832efc7ceedcd28b8ecc155f
kallebloomkwist <kallebloom@yandex.ru>
2022-04-01 05:33:27 +0300

    Add problem 278

---
+++ 278_first_bad_version.py
@@ -0,0 +1,23 @@
+# The isBadVersion API is already defined for you.
+def isBadVersion(version):
+    pass
+
+
+class Solution:
+    def firstBadVersion(self, n):
+        '''
+        Finds first bad version in release sequence from 1 to n
+        :param n: number of versions
+        :return: bad, number of the first bad version
+        '''
+        left, right = 1, n
+        # Corner case
+        if isBadVersion(left):
+            return 1
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid
+        return right
