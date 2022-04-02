cd01debdb8f5d3908552ba9efba76b70084994fa
kallebloomkwist <kallebloom@yandex.ru>
2022-04-02 07:20:10 +0300

    Add solution

---
+++ 1_two_sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums, target):
+        '''
+        Finds indices of 2 elements, with sum equal to another element of list, there's always one solution
+        :param nums: array of integers
+        :param target: target integer, should be equal to another two elements of nums
+        :return: indices of these two elements
+        '''
+        dic = {}
+        for i, num in enumerate(nums):
+            dif = target - num
+            if dif in dic:
+                return [dic[dif], i]
+            dic[num] = i
